72e897008482cda4980188fbb129beff22ac75c4
Daniel Ferrier <dwlferrier2002@gmail.com>
2024-01-08 17:15:20 +0000

    removed sql parts of buildfile and stripped catalogue to base

--- Buildfile
+++ Buildfile
@@ -1,9 +1,6 @@
-docker build -t flaskapp .
-docker tag flaskapp acobley/flaskapp
-docker push acobley/flaskapp
-docker stop flaskapp
-docker rm flaskapp
-docker stop some-mysql
-docker rm some-mysql
-docker run -p 3306:3306 --name some-mysql -e MYSQL_ROOT_PASSWORD=dacjd156n. -d mysql:8.0
-docker run -p 80:5000 -d --name flaskapp --link some-mysql:some-mysql acobley/flaskapp
+docker build -t myflix .
+docker tag myflix dwlferrier/myflix
+docker push dwlferrier/myflix
+docker stop myflix
+docker rm myflix
+docker run -p 80:5000 -d --name myflix dwlferrier/myflix
--- catalogue.py
+++ catalogue.py
@@ -14,16 +14,17 @@ app.debug = True
 @app.route('/Video/<video>')
 def video_page(video):
     print (video)
+    #TODO: this IP is for mongoDB server
     url = 'http://34.173.227.154/myflix/videos?filter={"video.uuid":"'+video+'"}'
     headers = {}
     payload = json.dumps({ })
     print (request.endpoint)
     response = requests.get(url)
     print (url)
+    jResp = response.json()
     if response.status_code != 200:
       print("Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message']))
       return "Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message'])
-    jResp = response.json()
     print (type(jResp))
     print (jResp)
     for index in jResp:
@@ -42,42 +43,43 @@ def video_page(video):
 
 @app.route('/')
 def cat_page():
-    url = "http://34.173.227.154/myflix/videos"
-    headers = {}
-    payload = json.dumps({ })
-
-    response = requests.get(url)
-    #print (response)
-    # exit if status code is not ok
-    print (response)
-    print (response.status_code)
-    if response.status_code != 200:
-      print("Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message']))
-      return "Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message'])
-    jResp = response.json()
-    print (type(jResp))
+    #TODO: this IP is for mongoDB server
+    # url = "http://34.173.227.154/myflix/videos"
+    # headers = {}
+    # payload = json.dumps({ })
+    #
+    # response = requests.get(url)
+    # #print (response)
+    # # exit if status code is not ok
+    # print (response)
+    # print (response.status_code)
+    # jResp = response.json()
+    # if response.status_code != 200:
+    #   print("Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message']))
+    #   return "Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message'])
+    # print (type(jResp))
     html="<h2> Your Videos</h2>"
-    for index in jResp:
-       #print (json.dumps(index))
-       print ("----------------")
-       for key in index:
-
-           if (key !="_id"):
-              print (index[key])
-              for key2 in index[key]:
-                  print (key2,index[key][key2])
-                  if (key2=="Name"):
-                      name=index[key][key2]
-                  if (key2=="thumb"):
-                      thumb=index[key][key2]
-                  if (key2=="uuid"):
-                      uuid=index[key][key2]  
-              html=html+'<h3>'+name+'</h3>'
-              ServerIP=request.host.split(':')[0]
-              html=html+'<a href="http://'+ServerIP+'/Video/'+uuid+'">'
-              html=html+'<img src="http://35.228.145.155/pics/'+thumb+'">'
-              html=html+"</a>"        
-              print("=======================")
+    # for index in jResp:
+    #    #print (json.dumps(index))
+    #    print ("----------------")
+    #    for key in index:
+    #
+    #        if (key !="_id"):
+    #           print (index[key])
+    #           for key2 in index[key]:
+    #               print (key2,index[key][key2])
+    #               if (key2=="Name"):
+    #                   name=index[key][key2]
+    #               if (key2=="thumb"):
+    #                   thumb=index[key][key2]
+    #               if (key2=="uuid"):
+    #                   uuid=index[key][key2]
+    #           html=html+'<h3>'+name+'</h3>'
+    #           ServerIP=request.host.split(':')[0]
+    #           html=html+'<a href="http://'+ServerIP+'/Video/'+uuid+'">'
+    #           html=html+'<img src="http://35.228.145.155/pics/'+thumb+'">'
+    #           html=html+"</a>"
+    #           print("=======================")
 
     return html
 
